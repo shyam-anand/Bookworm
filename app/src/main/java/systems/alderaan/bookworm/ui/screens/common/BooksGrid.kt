f4c8213d1992484b9dd75b7076507f3dcf1c673a
Shyam Anand <shyamwdr@gmail.com>
2023-05-02 13:02:18 +0530

    Using AsyncImagePainter.State to display book covers

--- app/src/main/java/systems/alderaan/bookworm/ui/screens/common/BooksGrid.kt
+++ app/src/main/java/systems/alderaan/bookworm/ui/screens/common/BooksGrid.kt
@@ -12,12 +12,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.shyamanand.bookworm.R
 import systems.alderaan.bookworm.data.model.Book
 import systems.alderaan.bookworm.network.model.SearchResultItem
@@ -29,10 +34,10 @@ fun BooksGrid(
     modifier: Modifier = Modifier
 ) {
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 120.dp),
+        columns = GridCells.Adaptive(minSize = 100.dp),
         modifier = modifier
             .fillMaxSize()
-            .padding(start = 8.dp, end = 8.dp)
+            .padding(start = 4.dp, end = 4.dp)
     ) {
         items(
             items = books,
@@ -59,19 +64,35 @@ fun BookCover(
         onClick = { onClick(book.id) },
         shape = RectangleShape,
         modifier = modifier
-            .padding(start = 2.dp, end = 2.dp, top = 10.dp)
+            .padding(start = 2.dp, end = 2.dp, top = 2.dp)
             .size(height = height.dp, width = width.dp),
         colors = CardDefaults.cardColors(containerColor = Color.Transparent),
         elevation = CardDefaults.cardElevation(0.dp)
     ) {
         if (book.imageUrl != null && book.imageUrl.isNotEmpty()) {
-            AsyncImage(
-                model = book.imageUrl.replace("http:", "https:"),
-                contentDescription = stringResource(R.string.cover_image, book.title),
-                contentScale = ContentScale.Fit,
-                modifier = modifier.size(200.dp),
-                error = painterResource(R.drawable.no_image)
-            )
+            val imageUrl = book.imageUrl.replace("http:", "https:")
+            Row {
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(imageUrl)
+                        .size(Size.ORIGINAL)
+                        .crossfade(true)
+                        .build()
+                )
+
+                if (painter.state is AsyncImagePainter.State.Success) {
+                    AsyncImage(
+                        model = book.imageUrl.replace("http:", "https:"),
+                        contentDescription = stringResource(R.string.cover_image, book.title),
+                        contentScale = ContentScale.Fit,
+                        modifier = modifier.size(200.dp),
+                        error = painterResource(R.drawable.no_image),
+                        onLoading = {},
+                    )
+                } else {
+                    CoverPlaceholder(title = book.title, author = book.authors)
+                }
+            }
         } else {
             CoverPlaceholder(title = book.title, author = book.authors)
         }
